01eff8cf2fb1ebd8b8cfc2d4f1c9d732902e5898
Ford1508 <phuclc1508@gmail.com>
2023-03-05 22:23:06 +0700

    cofig ngrok

--- src/API/api.js
+++ src/API/api.js
@@ -1,2 +1,2 @@
-export const api = "danang-travel-be-production.up.railway.app/"
-export const api_image = "danang-travel-be-production.up.railway.app/"
+export const api = "https://danang-travel-be-production.up.railway.app/"
+export const api_image = "https://danang-travel-be-production.up.railway.app/"
