d5046f854ff3b77c88d1ea58febc9d7a75ea1ca1
Ford1508 <phuclc1508@gmail.com>
2023-03-05 21:46:50 +0700

    cofig ngrok

--- src/API/api.js
+++ src/API/api.js
@@ -1,2 +1,2 @@
-export const api = "https://3bf3-58-187-198-82.ap.ngrok.io/"
-export const api_image = "https://3bf3-58-187-198-82.ap.ngrok.io/"
+export const api = "https://9ad5-58-187-198-82.ap.ngrok.io/"
+export const api_image = "https://9ad5-58-187-198-82.ap.ngrok.io/"
